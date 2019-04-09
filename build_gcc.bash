c781ff456187f55ca6960ecfa7f10508ae9851b4
lixq <lixq@wangsu.com>
2019-04-09 10:21:47 +0800

    build gcc

--- build_gcc.bash
+++ build_gcc.bash
@@ -19,27 +19,31 @@ rm "$destdir/gcc"* -rf
 rm "$destdir/src/gcc"* -rf
 yum-builddep gcc -y
 cd "$destdir/src" || exit 1
-rm -f gcc-8.2.0.tar.gz*
-until wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz; do
-    rm -f gcc-8.2.0.tar.gz*
+rm -f gcc-8.3.0.tar.gz*
+until wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.gz; do
+    rm -f gcc-8.3.0.tar.gz*
 done
-tar -xf gcc-8.2.0.tar.gz
-cd "$destdir/src/gcc-8.2.0" || exit 1
-cp "$sh_dir/downloads/gmp-6.1.0.tar.bz2" .
-cp "$sh_dir/downloads/mpfr-3.1.4.tar.bz2" .
-cp "$sh_dir/downloads/mpc-1.0.3.tar.gz" .
-cp "$sh_dir/downloads/isl-0.18.tar.bz2" .
+tar -xf gcc-8.3.0.tar.gz
+cd "$destdir/src/gcc-8.3.0" || exit 1
+gmp='gmp-6.1.0.tar.bz2'
+mpfr='mpfr-3.1.4.tar.bz2'
+mpc='mpc-1.0.3.tar.gz'
+isl='isl-0.18.tar.bz2'
+cp "$sh_dir/downloads/$gmp" .
+cp "$sh_dir/downloads/$mpfr" .
+cp "$sh_dir/downloads/$mpc" .
+cp "$sh_dir/downloads/$isl" .
 ./contrib/download_prerequisites
-mkdir "$destdir/src/gcc-8.2.0/build"
-cd "$destdir/src/gcc-8.2.0/build" || exit 1
-../configure --prefix="$destdir/gcc-8.2.0" --disable-multilib
+mkdir "$destdir/src/gcc-8.3.0/build"
+cd "$destdir/src/gcc-8.3.0/build" || exit 1
+../configure --prefix="$destdir/gcc-8.3.0" --disable-multilib
 make
 make install
 cd ~ || exit 1
-if [[ -d "$destdir/gcc-8.2.0" ]]; then
+if [[ -d "$destdir/gcc-8.3.0" ]]; then
     rm "$destdir/src/gcc"* -rf
     cd "$destdir" || exit 1
-    ln -s gcc-8.2.0 gcc
+    ln -s gcc-8.3.0 gcc
     echo_info "build gcc success" >> "$destdir/src/install_from_src.log"
 else
     echo_info "build gcc failed" >> "$destdir/src/install_from_src.log"
