4fc5992d8148eb0d997f5b313eb006152ebe91dc
lixq <lixq@wangsu.com>
2019-05-05 15:41:47 +0800

    build gcc

--- build_gcc.bash
+++ build_gcc.bash
@@ -19,12 +19,16 @@ rm "$destdir/gcc"* -rf
 rm "$destdir/src/gcc"* -rf
 yum-builddep gcc -y
 cd "$destdir/src" || exit 1
-rm -f gcc-8.3.0.tar.gz*
-until wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.gz; do
-    rm -f gcc-8.3.0.tar.gz*
-done
-tar -xf gcc-8.3.0.tar.gz
-cd "$destdir/src/gcc-8.3.0" || exit 1
+if [[ -f "$sh_dir/downloads/gcc-9.1.0.tar.gz" ]]; then
+    tar -xf "$sh_dir/downloads/gcc-9.1.0.tar.gz"
+else
+    rm -f gcc-9.1.0.tar.gz*
+    until wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-9.1.0/gcc-9.1.0.tar.gz; do
+        rm -f gcc-9.1.0.tar.gz*
+    done
+    tar -xf gcc-9.1.0.tar.gz
+fi
+cd "$destdir/src/gcc-9.1.0" || exit 1
 gmp='gmp-6.1.0.tar.bz2'
 mpfr='mpfr-3.1.4.tar.bz2'
 mpc='mpc-1.0.3.tar.gz'
@@ -34,16 +38,16 @@ cp "$sh_dir/downloads/$mpfr" .
 cp "$sh_dir/downloads/$mpc" .
 cp "$sh_dir/downloads/$isl" .
 ./contrib/download_prerequisites
-mkdir "$destdir/src/gcc-8.3.0/build"
-cd "$destdir/src/gcc-8.3.0/build" || exit 1
-../configure --prefix="$destdir/gcc-8.3.0" --disable-multilib
+mkdir "$destdir/src/gcc-9.1.0/build"
+cd "$destdir/src/gcc-9.1.0/build" || exit 1
+../configure --prefix="$destdir/gcc-9.1.0" --disable-multilib
 make
 make install
 cd ~ || exit 1
-if [[ -d "$destdir/gcc-8.3.0" ]]; then
+if [[ -d "$destdir/gcc-9.1.0" ]]; then
     rm "$destdir/src/gcc"* -rf
     cd "$destdir" || exit 1
-    ln -s gcc-8.3.0 gcc
+    ln -s gcc-9.1.0 gcc
     echo_info "build gcc success" >> "$destdir/src/install_from_src.log"
 else
     echo_info "build gcc failed" >> "$destdir/src/install_from_src.log"
