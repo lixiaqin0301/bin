e2a310e28c8d4791bf0de22220a1a1c8ebd89bc1
lixq <lixq@wangsu.com>
2019-08-29 09:31:37 +0800

    build python3

--- build_python3.bash
+++ build_python3.bash
@@ -21,27 +21,27 @@ rm "$destdir/src/Python-3"* -rf
 yum-builddep python -y
 $dnfyum install openssl openssl-devel libffi libffi-devel -y
 cd "$destdir/src" || exit 1
-if [[ -f "$sh_dir/downloads/Python-3.7.3.tar" ]]; then
-    tar -xf "$sh_dir/downloads/Python-3.7.3.tar"
+if [[ -f "$sh_dir/downloads/Python-3.7.4.tar" ]]; then
+    tar -xf "$sh_dir/downloads/Python-3.7.4.tar"
 else
-    rm -f Python-3.7.3.tar.xz*
-    until wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz; do
-        rm -f Python-3.7.3.tar.xz*
+    rm -f Python-3.7.4.tar.xz*
+    until wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz; do
+        rm -f Python-3.7.4.tar.xz*
     done
-    xz -d Python-3.7.3.tar.xz
-    tar -xf Python-3.7.3.tar
+    xz -d Python-3.7.4.tar.xz
+    tar -xf Python-3.7.4.tar
 fi
-mkdir "$destdir/src/Python-3.7.3/build"
-cd "$destdir/src/Python-3.7.3/build" || exit 1
+mkdir "$destdir/src/Python-3.7.4/build"
+cd "$destdir/src/Python-3.7.4/build" || exit 1
 export LD_RUN_PATH=$destdir/Python3/lib
-../configure --prefix=$destdir/Python-3.7.3 --enable-shared
+../configure --prefix=$destdir/Python-3.7.4 --enable-shared
 make
 make install
 cd ~ || exit 1
-if [[ -d "$destdir/Python-3.7.3" ]]; then
+if [[ -d "$destdir/Python-3.7.4" ]]; then
     rm "$destdir/src/Python-3"* -rf
     cd "$destdir" || exit 1
-    ln -s Python-3.7.3 Python3
+    ln -s Python-3.7.4 Python3
     echo_info "build Python3 success" >> "$destdir/src/install_from_src.log"
 else
     echo_info "build Python3 failed" >> "$destdir/src/install_from_src.log"
