550e5ba5347c8721d15726eb338c185f8ca4c92f
lixq <lixq@wangsu.com>
2019-10-02 18:20:02 +0800

    d

--- download_upgrade_rpms.sh
+++ download_upgrade_rpms.sh
@@ -9,5 +9,5 @@ rm -rfv /tmp/r
 mkdir /tmp/r
 cd /tmp/r || exit 1
 
-awk 'BEGIN{printf "wget "} NF>4{printf "https://mirrors4.tuna.tsinghua.edu.cn/fedora/updates/30/Everything/x86_64/Packages/%s/%s-%s.%s.rpm ",tolower(substr($1,1,1)),$1,$3,$2}' "$filepath" > 1.sh
+awk 'BEGIN{printf "wget "} NF>4{printf "http://mirrors.ustc.edu.cn/fedora/updates/30/Everything/x86_64/Packages/%s/%s-%s.%s.rpm ",tolower(substr($1,1,1)),$1,$3,$2}' "$filepath" > 1.sh
 bash 1.sh
