a4b54d4b306645f04b69180c9987d36700ef2834
lixq <lixq@wangsu.com>
2019-10-08 09:29:26 +0800

    w

--- backup.bash
+++ backup.bash
@@ -7,13 +7,3 @@ rsync -av --delete -e 'ssh -p 63501' /mnt/lixq/vod/Work root@lixq-el7:/home/lixq
 rsync -av --delete -e 'ssh -p 63501' /mnt/lixq/vod/sbin root@lixq-el5:/home/lixq/backup/
 rsync -av --delete -e 'ssh -p 63501' /mnt/lixq/vod/sbin root@lixq-el6:/home/lixq/backup/
 rsync -av --delete -e 'ssh -p 63501' /mnt/lixq/vod/sbin root@lixq-el7:/home/lixq/backup/
-sb=$(ls /mnt/lixq/vod/trunk/ | tail -n 1)
-rsync -arv -e 'ssh -p 63501' /mnt/lixq/vod/trunk/$sb /mnt/lixq/vod/lixq/$sb* root@lixq-el5:/root/vod/
-rsync -arv -e 'ssh -p 63501' /mnt/lixq/vod/trunk/$sb /mnt/lixq/vod/lixq/$sb* root@lixq-el6:/root/vod/
-rsync -arv -e 'ssh -p 63501' /mnt/lixq/vod/trunk/$sb /mnt/lixq/vod/lixq/$sb* root@lixq-el7:/root/vod/
-
-for i in 5 6 7; do
-    echo lixq-el$i
-    ssh root@lixq-el$i -p 63501 'du -csh /home/lixq/backup/*'
-    echo
-done
--- work.bash
+++ work.bash
@@ -4,20 +4,20 @@ sh_name="$(basename "${BASH_SOURCE[0]}")"
 sh_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1; pwd)"
 export sh_full_path="$sh_dir/$sh_name"
 
-if ! killall -0 rdm; then
-    rm -rfv /tmp/rdm
-    mkdir /tmp/rdm
-    echo "mkdir /tmp/rdm"
-    cd /tmp/rdm || exit 1
-    setsid nohup rdm &
-fi
+#if ! killall -0 rdm; then
+#    rm -rfv /tmp/rdm
+#    mkdir /tmp/rdm
+#    echo "mkdir /tmp/rdm"
+#    cd /tmp/rdm || exit 1
+#    setsid nohup rdm &
+#fi
 
 if ! killall -q -0 rdesktop; then
     rm /tmp/rdesktop -rfv
     mkdir /tmp/rdesktop
     echo "mkdir /tmp/rdesktop"
     cd /tmp/rdesktop || exit 1
-    setsid nohup rdesktop -a 32 -g 1366x700 -r disk:myshare=/tmp/rdesktop -u lixq -p "$(awk '/^xp/{print $5}' /mnt/lixq/vod/sbin/ha.txt)" "$(awk '/^xp/{print $3}' /mnt/lixq/vod/sbin/ha.txt)" &
+    setsid nohup rdesktop -a 32 -g 1366x700 -r disk:myshare=/tmp/rdesktop -u lixq -p "$(awk '/^xp/{print $5}' "$sh_dir/../sbin/ha.txt")" "$(awk '/^xp/{print $3}' "$sh_dir/../sbin/ha.txt")" &
 fi
 
 if ! killall -q -0 eclipse; then
