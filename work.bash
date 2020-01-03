0d7a729b2c74ba926b1a6b7d5b7f73046fb83fca
lixq <lixq@wangsu.com>
2020-01-03 17:45:12 +0800

    w

--- work.bash
+++ work.bash
@@ -4,14 +4,6 @@ sh_name="$(basename "${BASH_SOURCE[0]}")"
 sh_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1; pwd)"
 export sh_full_path="$sh_dir/$sh_name"
 
-#if ! killall -0 rdm; then
-#    rm -rfv /tmp/rdm
-#    mkdir /tmp/rdm
-#    echo "mkdir /tmp/rdm"
-#    cd /tmp/rdm || exit 1
-#    setsid nohup rdm &
-#fi
-
 if ! killall -q -0 rdesktop; then
     rm /tmp/rdesktop -rfv
     mkdir /tmp/rdesktop
@@ -20,10 +12,10 @@ if ! killall -q -0 rdesktop; then
     setsid nohup rdesktop -a 32 -g 1366x705 -r disk:myshare=/tmp/rdesktop -u "$(awk '/^win/{print $2}' "$sh_dir/../sbin/ha.txt")" -p "$(awk '/^win/{print $5}' "$sh_dir/../sbin/ha.txt")" "$(awk '/^win/{print $3}' "$sh_dir/../sbin/ha.txt")" &
 fi
 
-#if ! killall -q -0 eclipse; then
-#    rm /tmp/eclipse -rfv
-#    mkdir /tmp/eclipse
-#    echo "mkdir /tmp/eclipse"
-#    cd /tmp/eclipse || exit 1
-#    setsid nohup eclipse &
-#fi
+if ! ps u -C WXWork.exe; then
+    rm /tmp/wxwork -rfv
+    mkdir /tmp/wxwork
+    echo "mkdir /tmp/wxwork"
+    cd /tmp/wxwork || exit 1
+    setsid nohup wine /root/.wine/drive_c/Program\ Files\ \(x86\)/WXWork/WXWork.exe &
+fi
