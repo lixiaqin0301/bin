a73afe0d868b44a8d47906feb268e4bb600c74a0
lixq <lixq@wangsu.com>
2020-05-09 17:17:56 +0800

    build vscode server

--- build_vscode_server.bash
+++ build_vscode_server.bash
@@ -1,12 +1,7 @@
 #!/bin/bash
-sh_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1; pwd)"
-cd "$sh_dir" || exit 1
+version=1.45.0
+commit=d69a79b73808559a91206d73d7717ff5f798f23c
 
-version=1.44.2
-commit=ff915844119ce9485abfe8aa9076ec76b5300ddd
-
-[[ -d "$sh_dir/downloads" ]] || mkdir -p "$sh_dir/downloads"
-cd "$sh_dir/downloads" || exit 1
 if [[ ! -f "vscode-server-${version}.linux-x64.tar.gz" ]]; then
     until wget "https://update.code.visualstudio.com/commit:${commit}/server-linux-x64/stable" -O "vscode-server-${version}.linux-x64.tar.gz"; do
         rm -rf "vscode-server-${version}.linux-x64.tar.gz"*
